624c0a735d95616ceab3dcadac32071dfabc4cb0
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-04 16:02:03 -0700

    use sub route

--- _notes/react-router.md
+++ _notes/react-router.md
@@ -1,1 +1,10 @@
-$ npm install -S react-router-dom
+## Dependencies
+  $ npm install -S react-router-dom
+
+
+## Sub route
+  React Router v4 nested routes props.children
+  https://stackoverflow.com/questions/43180088/react-router-v4-nested-routes-props-children/43504027
+
+  
+
--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 
-import Routes from './routes/index';
-
 import Template from './containers/Template'
-// import Home from './containers/Home'
-// import Profile from './containers/Profile'
+import Home from './containers/Home'
+import Profile from './containers/Profile'
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <div>
-        <Route path='/' component={Template} />
-    </div>
-  </Router>,
+  <Template>
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/'  component={Home} />
+        <Route path='/profile' component={Profile} />
+      </Switch>
+    </Router>
+  </Template>,
   document.getElementById('root')
 )
