9da01778391a3643f53ce6e53dbe90ff05095e17
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-04 21:33:11 -0700

    draw board

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react'
 import { Stage } from 'react-konva' 
+import {Board, Squares} from '../styled/TicTacToe'
 
 // the home page 
 export default class TicTacToe extends Component {
   state = {
-
+    rows: 3,
+    gameState: new Array(9).fill(false),
+    ownMark: 'X',
+    otherMark: 'O',
+    gameOver: false,
+    yourTurn: true,
+    winner: false,
+    win: false
   }
 
   componentWillMount() {
+    let height = window.innerHeight
+    let width = window.innerWidth
+    let size = (height < width) ? height * .8 : width * .8
+    let rows = this.state.rows
+    let unit = size / rows
+    let coordinates = []
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < rows; x++) {
+        coordinates.push([x * unit, y * unit])
+      }
+    }
 
+    this.setState({
+      size,
+      rows,
+      unit,
+      coordinates
+    })
   }
+
   move = () => {
 
   }
@@ -28,11 +54,31 @@ export default class TicTacToe extends Component {
 
 
   render() {
+    let {
+      size,
+      unit,
+      rows,
+      coordinates,
+      gameState,
+      win,
+      gameOver,
+      yourTurn,
+      ownMark
+    } = this.state;
+
     return (
       <div>
-        <Stage>
-          {/*Board */}
-          {/*Squares*/}
+        <Stage width={size} height={size}>
+        <Board unit={unit} rows={rows} size={size} />
+        <Squares
+            unit={unit}
+            coordinates={coordinates}
+            gameState={gameState}
+            win={win}
+            gameOver={gameOver}
+            yourTurn={yourTurn}
+            ownMark={ownMark}
+            move={this.move} />
         </Stage>    
       </div>
     );
